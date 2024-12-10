4dd65776700b7a93a11927c9669422eced3a9892
leejun <hubo.move@gmail.com>
2024-12-10 23:16:15 +0900

    add GetBlock, SetBlock method

--- Matrix.cs
+++ Matrix.cs
@@ -1,9 +1,34 @@
-﻿using System;
+﻿/*
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2015 darkdragon-001
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+*/
+
+using System;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 
-namespace LightweightMatrixCSharp {
+namespace LightweightMatrixCSharp
+{
     public class Matrix
     {
         public int rows;
@@ -24,17 +49,22 @@ namespace LightweightMatrixCSharp {
 
         public Matrix(double[,] matrix, bool rowMajor = true)
         {
-            if (rowMajor) {
+            if (rowMajor)
+            {
                 rows = matrix.GetLength(0);
                 cols = matrix.GetLength(1);
                 mat = matrix.Cast<double>().ToArray();  // NOTE copies elements
-            } else {  // column-major
+            }
+            else
+            {  // column-major
                 rows = matrix.GetLength(1);
                 cols = matrix.GetLength(0);
                 mat = new double[rows * cols];
 
-                for (int row = 0; row < rows; row++) {
-                    for (int col = 0; col < cols; col++) {
+                for (int row = 0; row < rows; row++)
+                {
+                    for (int col = 0; col < cols; col++)
+                    {
                         this[row, col] = matrix[matrix.GetLowerBound(0) + col, matrix.GetLowerBound(1) + row];
                     }
                 }
@@ -64,6 +94,41 @@ namespace LightweightMatrixCSharp {
             for (int i = 0; i < rows; i++) this[i, k] = v[i, 0];
         }
 
+        public Matrix GetBlock(int startRow, int startCol, int cols, int rows)
+        {
+            if (startRow + rows > this.rows || startCol + cols > this.cols)
+            {
+                throw new MException("GetBlock: out of bounds");
+            }
+            Matrix m = new Matrix(rows, cols);
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    m[i, j] = this[startRow + i, startCol + j];
+                }
+            }
+            return m;
+        }
+        public Matrix SetBlock(int startRow, int startCol, int cols, int rows, Matrix m)
+        {
+            if (startRow + rows > this.rows || startCol + cols > this.cols)
+            {
+                throw new MException("SetBlock: out of bounds");
+            }
+            if (m.rows != rows || m.cols != cols)
+            {
+                throw new MException("SetBlock: given matrix dimensions not match");
+            }
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    this[startRow + i, startCol + j] = m[i, j];
+                }
+            }
+            return this;
+        }
         public void MakeLU()                        // Function for LU decomposition
         {
             if (!IsSquare()) throw new MException("The matrix is not square!");
@@ -255,11 +320,11 @@ namespace LightweightMatrixCSharp {
             return matrix;
         }
 
-        public static Matrix IdentityMatrix(int iRows, int iCols)   // Function generates the identity matrix
+        public static Matrix IdentityMatrix(int iRows, int iCols, double value = 1)   // Function generates the identity matrix
         {
             Matrix matrix = ZeroMatrix(iRows, iCols);
             for (int i = 0; i < Math.Min(iRows, iCols); i++)
-                matrix[i, i] = 1;
+                matrix[i, i] = value;
             return matrix;
         }
 
@@ -539,19 +604,23 @@ namespace LightweightMatrixCSharp {
                 return StupidMultiply(m1, m2);
             }
             // stupid multiplication faster for non square matrices
-            if (!m1.IsSquare() || !m2.IsSquare()) {
+            if (!m1.IsSquare() || !m2.IsSquare())
+            {
                 return StupidMultiply(m1, m2);
             }
             // Strassen multiplication is faster for large square matrix 2^N x 2^N
             // NOTE because of previous checks msize == m1.cols == m1.rows == m2.cols == m2.cols
             double exponent = Math.Log(msize) / Math.Log(2);
-            if (Math.Pow(2,exponent) == msize) {
+            if (Math.Pow(2, exponent) == msize)
+            {
                 return StrassenMultiply(m1, m2);
-            } else {
+            }
+            else
+            {
                 return StupidMultiply(m1, m2);
             }
         }
-        private static Matrix Multiply(double n, Matrix m)                          // Multiplication by constant n
+        private static Matrix Multiply(double n, Matrix m)  // Multiplication by constant n
         {
             Matrix r = new Matrix(m.rows, m.cols);
             for (int i = 0; i < m.rows; i++)
@@ -559,7 +628,7 @@ namespace LightweightMatrixCSharp {
                     r[i, j] = m[i, j] * n;
             return r;
         }
-        private static Matrix Add(Matrix m1, Matrix m2)         // Sčítání matic
+        private static Matrix Add(Matrix m1, Matrix m2) // Sčítání matic
         {
             if (m1.rows != m2.rows || m1.cols != m2.cols) throw new MException("Matrices must have the same dimensions!");
             Matrix r = new Matrix(m1.rows, m1.cols);
@@ -569,7 +638,7 @@ namespace LightweightMatrixCSharp {
             return r;
         }
 
-        public static string NormalizeMatrixString(string matStr)	// From Andy - thank you! :)
+        public static string NormalizeMatrixString(string matStr)   // From Andy - thank you! :)
         {
             // Remove any multiple spaces
             while (matStr.IndexOf("  ") != -1)
